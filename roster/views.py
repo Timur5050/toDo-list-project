3d92a515d1f90aba8cbd16fe7292b6fc89c643f4
pro <pro@pros-MacBook-Pro.local>
2024-07-08 17:22:58 +0300

    created main page and registration view

--- roster/views.py
+++ roster/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from roster.forms import UserForm
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "roster/index.html")
+
+
+class UserCreateView(CreateView):
+    form_class = UserForm
+    template_name = "roster/user_form.html"
+    success_url = reverse_lazy("roster:main-page")
