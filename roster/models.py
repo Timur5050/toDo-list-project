ad407b1e8cfa776065dfb95700cbd14f36e671f6
timur <stukantimur811@gmail.com>
2024-07-08 10:17:48 +0300

    created user model inherited from abstractuser

--- roster/models.py
+++ roster/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -8,3 +9,11 @@ class Tag(models.Model):
 class Occupation(models.Model):
     name = models.CharField(max_length=100)
 
+
+class User(AbstractUser):
+    occupation = models.ForeignKey(Occupation, on_delete=models.CASCADE, null=True, blank=True)
+
+    def set_occupation(self, occupation_name):
+        occupation, create = Occupation.objects.get_or_create(name=occupation_name)
+        self.occupation = occupation
+        self.save()
