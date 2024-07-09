c25ffbaa553572e15af09c0c58122415ee129dcf
pro <pro@pros-MBP.bbrouter>
2024-07-09 17:42:07 +0300

    created endpoint of all tasks view and for creation tasks

--- roster/urls.py
+++ roster/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 
-from roster.views import index, UserCreateView
+from roster.views import (
+    index,
+    UserCreateView,
+    TaskListView,
+    TaskCreateView
+)
 
 
 urlpatterns = [
     path("", index, name="main-page"),
-    path("registration/", UserCreateView.as_view(), name="registration")
+    path("registration/", UserCreateView.as_view(), name="registration"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
 ]
 
 app_name = "roster"
