b1c0691ddb1bef3a3f9d9efb2e5ebe924c032529
pro <pro@pros-MBP.bbrouter>
2024-07-09 17:42:59 +0300

    created views of tasks creation and tasks representation

--- roster/views.py
+++ roster/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth import login
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
 
-from roster.forms import UserForm
+from roster.forms import UserForm, TaskForm
+from roster.models import Task
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -14,3 +17,22 @@ class UserCreateView(CreateView):
     form_class = UserForm
     template_name = "roster/user_form.html"
     success_url = reverse_lazy("roster:main-page")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = self.object
+
+        login(self.request, user)
+
+        return response
+
+
+class TaskListView(ListView):
+    model = Task
+    fields = "__all__"
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("roster:task-list")
