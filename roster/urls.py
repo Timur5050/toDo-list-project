0e64eaaaa023279f9d390e9a027231b41567b2f2
pro <pro@pros-MBP.bbrouter>
2024-07-09 18:08:28 +0300

    created endpoint for user profile

--- roster/urls.py
+++ roster/urls.py
@@ -4,13 +4,15 @@ from roster.views import (
     index,
     UserCreateView,
     TaskListView,
-    TaskCreateView
+    TaskCreateView,
+    user_about
 )
 
 
 urlpatterns = [
     path("", index, name="main-page"),
     path("registration/", UserCreateView.as_view(), name="registration"),
+    path("users/<str:username>/", user_about, name="user-info"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
 ]
