f6470a0a6ee23056536b8711590042afdd206bbd
pro <pro@pros-MBP.bbrouter>
2024-07-09 18:09:15 +0300

    created view for user profile

--- roster/views.py
+++ roster/views.py
@@ -1,12 +1,12 @@
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse
 from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
 
 from roster.forms import UserForm, TaskForm
-from roster.models import Task
+from roster.models import Task, User
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -25,6 +25,14 @@ class UserCreateView(CreateView):
         login(self.request, user)
 
         return response
+
+
+def user_about(request: HttpRequest, username: str) -> HttpResponse:
+    user = get_object_or_404(User, username=username)
+    context = {
+        "user": request.user
+    }
+    return render(request, "roster/user_info.html", context=context)
 
 
 class TaskListView(ListView):
