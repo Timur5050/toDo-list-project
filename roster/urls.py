ccf466b52ecc6fb1f023c15d02c5c6d9c87d73c4
pro <pro@pros-MBP.bbrouter>
2024-07-11 09:34:33 +0300

    created endpoints to create and get adll list of tasks and tag

--- roster/urls.py
+++ roster/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from roster.views import (
     index,
     UserCreateView,
-    TaskListView,
-    TaskCreateView,
-    user_about
+    tasks_list,
+    task_create_view,
+    user_about,
+    #UserUpdateView,
+    tag_create,
+    tags_list,
 )
 
 
@@ -13,8 +16,12 @@ urlpatterns = [
     path("", index, name="main-page"),
     path("registration/", UserCreateView.as_view(), name="registration"),
     path("users/<str:username>/", user_about, name="user-info"),
-    path("tasks/", TaskListView.as_view(), name="task-list"),
-    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    #path("users/<int:pk>/update/", UserUpdateView.as_view(), name="user-update"),
+    path("tasks/", tasks_list, name="task-list"),
+    path("tasks/create/", task_create_view, name="task-create"),
+
+    path("tags/", tags_list, name="tags-list"),
+    path("tags/create/", tag_create, name="tag-create"),
 ]
 
 app_name = "roster"
