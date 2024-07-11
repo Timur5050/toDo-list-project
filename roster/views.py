58370f95779d5970f036ebf95f83b3cceea24dd7
pro <pro@pros-MBP.bbrouter>
2024-07-11 09:36:04 +0300

    created views to create and get list of related tags and tasks

--- roster/views.py
+++ roster/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpRequest, HttpResponse
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView
 from django.urls import reverse_lazy
 
-from roster.forms import UserForm, TaskForm
-from roster.models import Task, User
+from roster.forms import UserForm, TaskForm, TagForm
+from roster.models import Task, User, Tag
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -30,17 +31,56 @@ class UserCreateView(CreateView):
 def user_about(request: HttpRequest, username: str) -> HttpResponse:
     user = get_object_or_404(User, username=username)
     context = {
-        "user": request.user
+        "user": user
     }
     return render(request, "roster/user_info.html", context=context)
 
 
-class TaskListView(ListView):
-    model = Task
-    fields = "__all__"
+# class UserUpdateView(LoginRequiredMixin, UpdateView):
+#     model = User
+#     form_class = UserForm
+#     template_name = "roster/user_form.html"
+#     #success_url = reverse_lazy("roster:user-info", )
+@login_required
+def tag_create(request: HttpRequest) -> HttpResponse:
+    context = {
+        "form": TagForm
+    }
+    if request.method == "GET":
+        return render(request, "roster/tag_form.html", context=context)
+    if request.method == "POST":
+        form = TagForm(request.POST)
+        if form.is_valid():
+            tag = form.save(commit=False)
+            tag.author = request.user
+            tag.save()
+            return redirect(reverse_lazy("roster:tags-list"))
 
 
-class TaskCreateView(LoginRequiredMixin, CreateView):
-    model = Task
-    form_class = TaskForm
-    success_url = reverse_lazy("roster:task-list")
+def tags_list(request: HttpRequest) -> HttpResponse:
+    context = {
+        "tags_list": Tag.objects.filter(author=request.user)
+    }
+    return render(request, "roster/tags_list.html", context=context)
+
+
+def tasks_list(request: HttpRequest) -> HttpResponse:
+    context = {
+        "task_list": Task.objects.filter(user=request.user)
+    }
+    return render(request, "roster/task_list.html", context=context)
+
+
+def task_create_view(request: HttpRequest) -> HttpResponse:
+    if request.method == "GET":
+        context = {
+            "form": TaskForm(current_user=request.user)
+        }
+        return render(request, "roster/task_form.html", context=context)
+    if request.method == "POST":
+        form = TaskForm(request.POST, current_user=request.user)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect(reverse_lazy("roster:task-list"))
