3253ec338e17063182383dfc3e495997ffbf08d2
pro <pro@pros-MacBook-Pro.local>
2024-07-12 10:55:01 +0300

    added admin panel:

--- roster/admin.py
+++ roster/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from roster.models import Task, Tag, User, Occupation
+
+
+admin.site.register(Task)
+admin.site.register(Tag)
+admin.site.register(User)
+admin.site.register(Occupation)
