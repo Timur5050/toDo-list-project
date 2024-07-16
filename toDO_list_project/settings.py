64ac50f71626b5672dc91931770b53681e0b82bf
pro <pro@pros-MacBook-Pro.local>
2024-07-16 10:23:52 +0300

    static files managing

--- toDO_list_project/settings.py
+++ toDO_list_project/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 import dj_database_url
+from os import getenv
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -82,9 +85,10 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = "postgresql://neondb_owner:B4UE5uqHwSDO@ep-bold-mode-a5fdirod.us-east-2.aws.neon.tech/neondb?sslmode=require"
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
+
+DATABASE_URL = "postgresql://neondb_owner:B4UE5uqHwSDO@ep-bold-mode-a5fdirod.us-east-2.aws.neon.tech/neondb?sslmode=require"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -122,7 +126,7 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "staticfiles/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
